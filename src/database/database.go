24f1b46a1ed470ce2cb5d65cc782e3fcacc9801b
vabd <vabd@informo.network>
2019-01-04 10:06:23 +0000

    Add debug logs in DB management

--- src/database/database.go
+++ src/database/database.go
@@ -8,6 +8,8 @@ import (
 	// Database drivers
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/sirupsen/logrus"
 )
 
 // Database represents the crawler's database.
@@ -38,6 +40,10 @@ func NewDatabase(cfg *config.Config) (database *Database, err error) {
 // no saved state for this proposal.
 // Returns an error if we couldn't talk to the database.
 func (d *Database) UpdateProposalState(number int64, labels []string) error {
+	logrus.WithFields(logrus.Fields{
+		"number": number,
+		"labels": labels,
+	}).Debug("Updating proposal state")
 	return d.proposalState.upsertState(number, labels)
 }
 
@@ -45,5 +51,8 @@ func (d *Database) UpdateProposalState(number int64, labels []string) error {
 // no state has been saved for this proposal.
 // Returns an error if we couldn't talk to the database.
 func (d *Database) GetProposalState(number int64) ([]string, error) {
+	logrus.WithFields(logrus.Fields{
+		"number": number,
+	}).Debug("Retrieving proposal state")
 	return d.proposalState.selectState(number)
 }
