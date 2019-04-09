76d750a43d4b4939bf83a29a14a7216b0315843f
vabd <vabd@informo.network>
2019-04-09 14:16:12 +0100

    Add global lock to avoid multiple goroutines using the mutexes map at the same time

--- src/mutex/mutex.go
+++ src/mutex/mutex.go
@@ -4,11 +4,17 @@ import (
 	"sync"
 )
 
-var mutexes = make(map[int64]*sync.Mutex)
+var (
+	mutexes = make(map[int64]*sync.Mutex)
+	lock    = new(sync.Mutex)
+)
 
 // Lock locks the mutex for a given proposal, after instanciating if it doesn't
 // exist.
 func Lock(number int64) {
+	// Avoid multiple goroutines using the map at the same time.
+	lock.Lock()
+
 	// Lock the existing mutex if there's one.
 	if m, exists := mutexes[number]; exists {
 		m.Lock()
@@ -18,10 +24,15 @@ func Lock(number int64) {
 	// If there's no mutex for this proposal, create one then lock it.
 	mutexes[number] = new(sync.Mutex)
 	mutexes[number].Lock()
+
+	lock.Unlock()
 }
 
 // Unlock unlocks the mutex for a given proposal.
 // Panics if the mutex doesn't exist.
 func Unlock(number int64) {
+	// Avoid multiple goroutines using the map at the same time.
+	lock.Lock()
 	mutexes[number].Unlock()
+	lock.Unlock()
 }
